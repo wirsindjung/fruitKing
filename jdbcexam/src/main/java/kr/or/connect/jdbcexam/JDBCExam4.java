55c844fbe53eff672f76c940189615ba1e1c55d4
dolit <dolit@DESKTOP-TEB6TSD>
2018-08-20 21:51:18 +0900

    second commit

--- jdbcexam/src/main/java/kr/or/connect/jdbcexam/JDBCExam4.java
+++ jdbcexam/src/main/java/kr/or/connect/jdbcexam/JDBCExam4.java
@@ -5,7 +5,7 @@ import kr.or.connect.jdbcexam.dao.RoleDao;
 public class JDBCExam4 {
 	public static void main(String[] args) {
 //삭제 테스트
-		int roleId = 500;
+		int roleId = 900;
 
 		RoleDao dao = new RoleDao();
 		int deleteCount = dao.deleteRole(roleId);
