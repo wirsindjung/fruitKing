c0d6b32121e7abd8e95be4dce664e4fa51524451
leeminju <2mingju22@gmail.com>
2018-09-07 10:39:22 +0900

    .

--- fruitking/WebContent/test.php
+++ fruitking/WebContent/test.php
@@ -1,6 +1,5 @@
 <?php
-        $token = "AAAANySoOPyYR/q11j0A74wro79KexkT5vZmAFBTakupj3krySeq3Jxow68QZ+3w16PD+kVG2CIdqiVo4lXV1fS4c9c=";        //이곳에 엑세스
- 토큰을 넣어야 함
+        $token = "AAAAN51t0m0r5TF/GJ8jmFV5YZUxCA9T6cZtrt+MvIk7MrnKSRwkwZNRwNx6KnHxMSf06RYvL01OFG5m7+0XabwlRyg=";        //이곳에 엑세스 토큰을 넣어야 함
         $header = "Bearer ".$token; // Bearer 다음에 공백 추가
         $url = "https://openapi.naver.com/v1/nid/me";
         $is_post = false;
@@ -21,6 +20,8 @@
 
                 $json = json_decode($response, true);
                 $info = $json['response'];
+                echo "<br>".$response."<br><br>";
+                print_r($json);
                 echo "<br><br>회원 id : ".$info['id']."<br>이메일 : ".$info['email']."<br>이름 : ".$info['name']."<br>별명 : ".$info['nickname']."<br>나이 : ".$info['age']."<br>생일 : ".$info['birthday']."<br>성별 : ".$info['gender']."<br>프로필 이미지 경로 : ".$info['profile_image'];
 
         } else {
